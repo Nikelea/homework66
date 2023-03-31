79a8829edeae8ed3727bc09f95cd673dd73e702f
Гульняшкин Евгений <ducat.ea@gmail.com>
2023-03-31 14:21:47 +0600

    Task_1 completed.

---
+++ Task_1/script.js
@@ -0,0 +1,6 @@
+console.log(document.getElementById('string-3').childNodes[0].textContent);
+console.log(document.getElementById('string-5').childNodes[0].textContent);
+console.log(document.getElementById('string-2').childNodes[0].textContent);
+console.log(document.getElementById('string-6').childNodes[0].textContent);
+console.log(document.getElementById('string-4').childNodes[0].textContent);
+console.log(document.getElementById('string-1').childNodes[0].textContent);
